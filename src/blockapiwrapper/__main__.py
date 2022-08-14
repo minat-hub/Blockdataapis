0c4190c6c29ca0c6bf46532df751be6c20f924ce
minat-hub <minaomobonike@gmail.com>
2022-08-14 15:57:13 +0100

    explorer class updates

---
+++ src/blockapiwrapper/__main__.py
@@ -0,0 +1,12 @@
+from base import Explorer, Marketplace, Exchanges
+import pandas as pd
+
+def main():
+    scanner = Explorer("0xb38ef143BA4CDE9e66B5f93E23315E979e886D04", API_TOKEN)
+    data = scanner.etherscan_accounts("transactions")
+    return pd.DataFrame(data)
+
+
+if __name__ == "__main__":
+    main()
+   
