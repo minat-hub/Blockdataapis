8dc7f5a55def6d856d70183cae0079679dceeba8
minat-hub <minaomobonike@gmail.com>
2022-08-14 16:04:28 +0100

    main function updates

--- src/blockapiwrapper/__main__.py
+++ src/blockapiwrapper/__main__.py
@@ -1,5 +1,9 @@
 from base import Explorer, Marketplace, Exchanges
 import pandas as pd
+import requests
+from decouple import config
+
+API_TOKEN = config("etherscan_token", default="")
 
 def main():
     scanner = Explorer("0xb38ef143BA4CDE9e66B5f93E23315E979e886D04", API_TOKEN)
