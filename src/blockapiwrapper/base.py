499622b48d9c44c0e97fc221d89fad7a6bae8916
minat-hub <minaomobonike@gmail.com>
2022-09-11 23:25:39 +0100

    adds tests

--- src/blockapiwrapper/base.py
+++ src/blockapiwrapper/base.py
@@ -26,9 +26,22 @@ class Marketplace(Block):
 
 class Explorer(Block):
     def __init__(self, address, token):
+        """
+        The function __init__ is a constructor that takes two parameters, address and token, and calls
+        the superclass constructor with the same parameters.
+        
+        :param address: The address of the server
+        :param token: The token of the bot you want to use
+        """
         super(self.__class__, self).__init__(address, token)
 
     def etherscan_accounts(self, action):
+        """
+        It takes an action as a parameter and returns the result of the request
+        
+        :param action: The action you want to perform
+        :return: A list of dictionaries.
+        """
         if action == "transactions" or action not in [
             "internal_transactions",
             "balance",
@@ -47,9 +60,19 @@ class Explorer(Block):
                 f"https://api.etherscan.io/api?module=account&action=balancemulti&address={self.address}&tag=latest&apikey={self.token}"
             )
 
-        return response.json().get("result")
+        if response.status_code == 404:
+            raise Exception(f"Address {self.address} or Token {self.token} not found")
+        else:
+            return response.json().get("result")
 
     def etherscan_gastracker(self, action):
+        """
+        It takes in an action parameter, and if the action is "gas_history", it makes a POST request to
+        the Alchemy API, otherwise it makes a GET request to the Etherscan API
+        
+        :param action: gas_history
+        :return: The response.json() is being returned.
+        """
         if action == "gas_history":
             response = requests.post(
                 f"https://eth-mainnet.alchemyapi.io/v2/${self.token}"
@@ -59,7 +82,7 @@ class Explorer(Block):
                 f"https://api.etherscan.io/api?module=gastracker&action=gasprice&apikey={self.token}"
             )
 
-        return response.json()
+        return response.json().get("result")
 
     def etherscan_price(self):
         """
@@ -70,10 +93,8 @@ class Explorer(Block):
         response = requests.get(
             f"https://api.etherscan.io/api?module=stats&action=ethprice&apikey={self.token}"
         )
-        return response.json()
+        return response.json().get("result")
 
 
 class Exchanges(Block):
     pass
-
-
--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,11 @@
-   
+import pytest
+from decouple import config
+
+from src.blockapiwrapper.base import Explorer
+
+API_TOKEN = config("etherscan_token", default="")
+
+def test_string_calculator_should_return_zero_on_empty_string():
+    result = Explorer("0xb38ef143BA4CDE9e66B5f93E23315E979e886D04", API_TOKEN)
+    data = result.etherscan_accounts("transactions")
+    assert data is not None
